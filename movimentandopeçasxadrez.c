e67f0d931b0411736d60eef2c656453b073e22d3
Vitor Marlon <vitinhexasp.marlon@gmail.com>
2025-04-28 23:30:11 +0000

    Movimentações xadrex em C

---
+++ movimentandopeçasxadrez.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("\nJOGO DE XADREZ\n");
+    printf("\nMovimento torre, 5 casas para direita.\n"); // A torre se move em linha reta horizontalmente ou verticalmente.
+
+        for(int t = 0; t < 5; t++){  // Movimento da Torre ( for )
+
+            printf("Direita\n"); // Imprime a direção para a direita
+        }
+    
+    printf("\nMovimento bispo, 5 casas Cima, Direita.\n"); // O Bispo se move na diagonal, aqui vamos simular 5 casas
+    
+    int b = 0;
+        
+        while( b < 5 ){   // Movimento do Bispo ( while )
+            
+            printf("Cima, Direita\n");      // Imrpime a direção na diagonal para a direita
+            b++;
+        }
+    
+    printf("\nMovimento Rainha, 8 casas para a esquerda.\n"); // A Rainha se move em todas as direções, vamos simular 8 casas para esquerda
+
+    int r = 0;
+        
+        do{                     // Movimento da Rainha ( do while )
+            
+            printf("Esquerda\n"); // Imprime a direção para a esquerda
+            r++;
+        
+        } while ( r < 8);
+
+
+    return 0;
+
+}
